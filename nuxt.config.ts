dee07b554ef19054fef6115d5ba10537ddbb7de3
Qin Guan <qinguan20040914@gmail.com>
2023-12-16 11:46:07 +0800

    add clarity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,17 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: "viewport", content: "width=device-width, user-scalable=no" }
+      ],
+      script: [
+        { 
+          children: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "k6mixoq9xc");
+          ` 
+        }
       ]
     }
   },
